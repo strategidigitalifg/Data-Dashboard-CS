eee22064f4076e54a14fadd65c11d81e38d9318d
strategidigitalifg <strategidigitalifg@gmail.com>
2026-05-04 06:29:57 +0700

    Update main.py

--- main.py
+++ main.py
@@ -170,12 +170,11 @@ df_coster = df_coster[cols].rename(columns={
 df_coster["Created At Coster"] = pd.to_datetime(
     df_coster["Created At Coster"], format="%d/%m/%Y %H:%M:%S", errors="coerce"
 )
-df_coster["Assigned At"] = pd.to_datetime(
-    df_coster["Assigned At"], format="%d/%m/%Y %H:%M:%S", errors="coerce"
-)
+
 df_coster["First Response At"] = pd.to_datetime(
-    df_coster["First Response At"], format="%d/%m/%Y %H:%M:%S", errors="coerce"
+    df_coster["First Response At"], dayfirst=True, errors="coerce"
 )
+
 df_coster["Closed At Coster"] = pd.to_datetime(
     df_coster["Closed At Coster"], format="%d/%m/%Y %H:%M:%S", errors="coerce"
 )
